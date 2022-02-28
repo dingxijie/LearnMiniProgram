eb770e2a24a45354e8c83bdef1cdb652d684bcf4
dingxijie <1214034089@qq.com>
2022-02-28 21:25:18 +0800

    全局和页面配置_双线程模型_注册APP_注册Page

--- app.js
+++ app.js
@@ -1,2 +1,67 @@
-// app.js
-App({})
+// app.js 通过App函数注册一个小程序，并执行生命周期函数
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    console.log("小程序初始化完成时: onLaunch")
+
+    // 初始化时候 获取用户信息
+    wx.getUserInfo({
+      success: (res) => {
+        // console.log(res)
+      }
+    })
+
+    // 模拟发生错误
+    setTimeout(() => {
+      const err = new Error("发生了一些错误")
+      //throw err;
+    }, 3000)
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    console.log("小程序启动，或从后台进入前台显示: onShow")
+    // console.log(options)
+
+    /***  在生命周期函数里面，可能要做的几件事情，如下:
+     * 1: 判断小程序的进入场景
+     * 2. 监听生命周期函数，执行对应的业务逻辑，比如获取用户信息
+     * 3. 因为App()实例只有一个，并且是全局共享的(单例对象)，所以可以定义全局数据globalData，所有页面都可使用该数据
+     * */
+    // 1. 判断小程序的进入场景
+    switch (options.scene) {
+      case 1001:
+        break;
+      case 1005:
+        break;
+    }
+
+    // 2. 获取用户信息（3种方式获取，其中wx.getUserInfo方式已经废弃）
+    // 通常用<button open-type="getUserInfo" bindgetuserinfo="">获取用户授权</button>
+    // 用内置组件<open-data type="userNickName"></open-data>展示显示用户信息
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    console.log("小程序从前台进入后台: onHide")
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    console.log(msg);
+  },
+  globalData: {
+    name: 'dj',
+    age: 28
+  }
+})
+
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,80 @@
-// pages/home/home.js
+// getApp()可以获取app.js中，App()函数返回的实例对象，进而获取内部定义的东西
+const app = getApp();
+console.log(app.globalData.name)
+console.log(app.globalData.age)
+// 通过Page函数注册一个页面，并执行生命周期函数
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'DJ',
+    age: 18,
+    students: [
+      {name: 'dj1', age: 18},
+      {name: 'dj2', age: 19},
+      {name: 'dj3', age: 16},
+      {name: 'dj4', age: 13},
+    ],
+    counter: 0,
+    list: []
   },
-
   /**
-   * 生命周期函数--监听页面加载
+   * Page内部一般要做的几件事情
+   * 1. 监听生命周期函数，从服务器获取数据
+   * 2. 在data内初始化数据
+   * 3. 监听wxml中相关的一些事件
+   * 4. 监听其他事件，如上拉加载，下拉刷新，页面滚动等
    */
-  onLoad: function (options) {
-
+  onLoad() {
+    console.log("页面被加载出来 onLoad")
+    // 发送网络请求，获取数据
+    wx.request({
+      url: 'http://152.136.185.210:7878/api/hy66/recommend',
+      success: (res) => { // 这里用箭头函数，里面的this指向Page，没有问题。如果用function(res){},则this为undefied,需要再外部先用 _this=this
+        //console.log(res)
+        const data = res.data.data.list;
+        this.setData({
+          list: data
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onShow() {
+    console.log("当页面显示出来时 onShow")
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onReady() {
+    console.log("页面【初次】渲染完成时 onReady")
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  onHide() {
+    console.log("当页面隐藏起来时 onHide")
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onUnload() {
+    console.log("页面被卸载时 onUnload")
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  // 加一
+  addCount() {
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  // 减一
+  subCount() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 监听事件函数：获取用户信息
+  getUserInfo(event) {
+   console.log(event) 
+  },
+  // 监听页面滚动
+  onPageScroll(obj) {
+    // console.log(obj);
+  },
+  // 监听页面滚动到底部
+  onReachBottom() {
+    console.log("页面滚动到底部了")
+  },
+  // 监听下拉刷新:需要json内开启"enablePullDownRefresh": true
+  onPullDownRefresh() {
+    console.log("下拉刷新事件")
   }
 })
